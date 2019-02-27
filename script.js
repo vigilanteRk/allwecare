f200c241dc44259e92270f0afb40c3951fa692f1
vigilanteRk <vigilanterks@gmail.com>
2019-02-27 11:35:59 +0530

    more to do

---
+++ script.js
@@ -0,0 +1,5 @@
+function signin() {
+    var x = document.getElementById("inputEmail").value;
+    document.getElementById("demo").innerHTML = x;
+    alert("Hi "+ x + " thank you for Registration , you will be notified soon" )
+  }
